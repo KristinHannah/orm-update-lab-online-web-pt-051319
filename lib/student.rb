c92d43430e21a4b6848121277bf78bba6c5be5db
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-25 00:29:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,10 @@ class Student
  def save 
    sql = <<-SQL
    INSERT INTO students (id, name, grade) VALUES (?, ?, ?);
+   SQL
    
+   DB[:conn].execute(sql, self.name, self.album)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
  end 
  
 end
