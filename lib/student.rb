9f725cac5e86d397a23dcef64757d25511de2e38
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-25 00:29:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,8 +37,8 @@ class Student
    INSERT INTO students (id, name, grade) VALUES (?, ?, ?);
    SQL
    
-   DB[:conn].execute(sql, self.name, self.album)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+   DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
  end 
  
 end
