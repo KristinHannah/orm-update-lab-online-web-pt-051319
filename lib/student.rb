3c72df0a3835f71e13c9ace3ad8297e086bb9446
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-25 00:34:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
      self.update
   else 
    sql = <<-SQL
-   INSERT INTO students (name, grade) VALUES (?, ?, ?);
+   INSERT INTO students (name, grade) VALUES (?, ?);
    SQL
    
    DB[:conn].execute(sql, self.name, self.grade)
