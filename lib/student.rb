84decc443c286536a8afc87993548bb5ec599450
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-25 00:46:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,10 +58,10 @@ class Student
  end 
  
  def self.new_from_db(row)
+   id = row[0]
    name = row[1]
    grade = row[2]
-   new_student = Student.new(name, grade)
-   new_student.id = row[0]
+   new_student = Student.new(name, grade, id)
    new_student
  end 
  
